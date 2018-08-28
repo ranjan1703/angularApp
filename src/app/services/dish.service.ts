d9f6ad173df102d83f2fe0937699a1ad6ac1be34
ranjan1703 <rklsps17mpbr@gmail.com>
2018-08-28 22:23:21 +0530

    working with RxJS

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,28 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class DishService {
 
   constructor() { }
-getDishes():Promise<Dish[]>{
-  return new Promise(resolve=> {
-    //Simulate server latency with 2 sec delay
-    setTimeout(() => resolve(DISHES),2000);
-  });
-}
-
-  getDish(id: number): Promise<Dish>{
-    return new Promise(resolve=> {
-      //Simulate server latency with 2 sec delay
-      setTimeout(() => resolve(DISHES.filter((dish)=>(dish.id==id))[0]),2000);
-    });
+getDishes():Observable<Dish[]>{
+  return of(DISHES).pipe(delay(2000));
   }
-  getFeatureDish(): Promise<Dish>{
-    return new Promise(resolve=> {
-    setTimeout(() => resolve(DISHES.filter((dish)=>(dish.featured))[0]),2000);
-    });
+
+  getDish(id: number): Observable<Dish>{
+   
+      //Simulate server latency with 2 sec delay
+      return of(DISHES.filter((dish)=>(dish.id==id))[0]).pipe(delay(2000));
+    
+  }
+  getFeatureDish(): Observable<Dish>{
+    
+  return of(DISHES.filter((dish)=>(dish.featured))[0]).pipe(delay(2000));
+
 }
 }
